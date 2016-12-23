6d271a7246f275fcabfde1528e0f44cbb2f1f027
Kylan Hurt <kylan@datagoniaweb.com>
2016-12-22 21:11:48 -0800

    Enable User creation and query (all) endpoints, and also enable CORS

--- app/models/user.js
+++ app/models/user.js
@@ -1,26 +1,4 @@
 var mongoose = require('mongoose');
-/*mongoose.connect('mongodb://localhost:27017/larang');
-
-var UserSchema = new mongoose.Schema({
-	name: String,
-	email: String,
-	password: String,
-	remember_token: String,
-	created_at: Date,
-	updated_at: Date
-});
-
-var User = mongoose.model('User', UserSchema);
-
-var newUser = new User({'Kylan Hurt', 'kylan.hurt@gmail.com', 'Pu1ssant', 'asdfasdfasdfasdfsdf' });
-
-newUser.save(function(error) {
-	if(error){
-		console.log('newUser error:', error);
-	} else {
-		console.log('newUser:', newUser);
-	}
-});*/
 
 var Schema = mongoose.Schema;
 
--- server.js
+++ server.js
@@ -13,7 +13,10 @@ var router = express.Router();              // get an instance of the express Ro
 // middleware to use for all requests
 router.use(function(req, res, next) {
     // do logging
-    console.log('Something is happening.');
+    //console.log('Request is:', req, 'Response is:', res);
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	res.header('Access-Control-Allow-Headers', 'Content-Type');    
     next(); // make sure we go to the next routes and don't stop here
 });
 
@@ -25,8 +28,7 @@ router.get('/', function(req, res) {
 // more routes for our API will happen here
 
 // REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
+
 
 // START THE SERVER
 // =============================================================================
@@ -42,3 +44,32 @@ var User = require('./app/models/user');
 //API ROUTES
 //================================
 
+
+router.route('/users')
+	.post(function(req, res) {
+		var user = new User();
+		user.email = req.body.email; 
+		user.password = req.body.password;
+
+		user.save(function(err) {
+			if(err) {
+				res.send(err);
+			}
+
+			res.json({ message: "User created!" });
+		});
+	})
+
+	.get(function(req, res) {
+		User.find(function(err, users) {
+			if(err) {
+				res.send(err);
+			}
+
+			res.json(users);
+		});
+	});
+
+
+// all of our routes will be prefixed with /api
+app.use('/api', router);
